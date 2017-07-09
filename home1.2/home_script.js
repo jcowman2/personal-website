f2923c609338e7d69fc271d0b2acc0726e10d686
Joe Cowman <joe.cowman@huskers.unl.edu>
2017-07-09 17:40:12 -0500

    Added github activity to home1.2

--- home1.2/home_script.js
+++ home1.2/home_script.js
@@ -1,6 +1,12 @@
+GitHubActivity.feed({username: "jcowman2", selector: "#feed"});
+
 $(document).ready(function() {
+
   $("#resume").hide();
+
   $("#resumeBtn").click(function() {
+    $("#feed").hide();
     $("#resume").fadeIn();
   })
+
 })
