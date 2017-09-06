7a02b36a73c95fbee89d96e0acb5fced1019a8f3
Joe Cowman <joe.cowman@huskers.unl.edu>
2017-09-06 00:14:16 -0500

    Reorganization

---
+++ js/home/home-script.js
@@ -0,0 +1,21 @@
+GitHubActivity.feed({username: "jcowman2", selector: "#feed"});
+
+$(document).ready(function() {
+  console.log("barK");
+
+  $("#resume").hide();
+  $("#feed").hide();
+
+  $("#resumeBtn").click(function() {
+    $("#feed").hide();
+    $("#resume").fadeIn();
+  })
+
+  setTimeout(function() {
+    console.log(githubUserNotFound);
+    if (!githubUserNotFound) {
+      $("#feed").fadeIn();
+    }
+  }, 1000);
+
+})
