854a659db6e4a77f49b7b536cfcfcb1aecaba569
Joe Cowman <joe.cowman@huskers.unl.edu>
2017-09-24 14:26:33 -0500

    Cleaned up methods in portfolio-script

--- js/portfolio/portfolio-script.js
+++ js/portfolio/portfolio-script.js
@@ -3,20 +3,37 @@ $(document).ready(function() {
   $('.portfolio-item').click(function() {
     $(this).css('height', $(this).outerHeight()); //needed to allow animations to work the first click
 
-    var currentHeight = 0;
-    var $innerContent = $(this).find('.inner-content');
+    var $portfolioItem = $(this);
+    var $innerContent = $portfolioItem.find('.inner-content');
 
     if ($innerContent.css('display') == 'none') {
-      $innerContent.show();
-      currentHeight = $(this).find('.all-content').height();
-      $innerContent.hide().fadeIn();
+      expandTile($portfolioItem, $innerContent);
     } else {
-      $innerContent.hide();
-      currentHeight = $(this).find('.all-content').height();
+      collapseTile($portfolioItem, $innerContent);
     }
-
-    $(this).css('height', currentHeight);
-
   });
 
 })
+
+/*
+ * Expands portfolio tile to reveal inner content
+ * $portfolioItem  the .portfolio-item
+ * $innerContent   the .inner-content div of the .portfolio-item
+ */
+expandTile = function($portfolioItem, $innerContent) {
+  $innerContent.show();
+  var currentHeight = $portfolioItem.find('.all-content').height();
+  $innerContent.hide().fadeIn();
+  $portfolioItem.css('height', currentHeight);
+}
+
+/*
+ * Collapses portfolio tile to hide inner content
+ * $portfolioItem  the .portfolio-item
+ * $innerContent   the .inner-content div of the .portfolio-item
+ */
+collapseTile = function($portfolioItem, $innerContent) {
+  $innerContent.hide();
+  var currentHeight = $portfolioItem.find('.all-content').height();
+  $portfolioItem.css('height', currentHeight);
+}
