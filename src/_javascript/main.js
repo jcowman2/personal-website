1084a4e4d718dbadadf15a26fac89b2773d13a28
jcowman2 <joe.cowman@huskers.unl.edu>
2019-09-30 14:45:51 -0500

    Some responsiveness improvements

--- src/_javascript/main.js
+++ src/_javascript/main.js
@@ -1,3 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
+// import smoothscroll from "smoothscroll-polyfill";
+
+// smoothscroll.polyfill();
+
+const anchorAbout = document.querySelector("#_about");
+
+const btnEnterSite = document.querySelector("#btn-enter-site");
+btnEnterSite.onclick = () => {
+  console.log("yo");
+  // anchorAbout.scrollIntoView({ behavior: "smooth" });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Page loaded");
 });
--- src/lib/main.js
+++ src/lib/main.js
@@ -1,5 +1,17 @@
-'use strict';
+"use strict";
 
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('Hello Bulma!');
+// import smoothscroll from "smoothscroll-polyfill";
+
+// smoothscroll.polyfill();
+
+var anchorAbout = document.querySelector("#_about");
+
+var btnEnterSite = document.querySelector("#btn-enter-site");
+btnEnterSite.onclick = function () {
+  console.log("yo");
+  // anchorAbout.scrollIntoView({ behavior: "smooth" });
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Page loaded");
 });
